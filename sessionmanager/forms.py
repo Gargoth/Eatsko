258b8cc997c386f35913e4e362fdabf29ef198b4
gabvariable <gabvariable@github.com>
2023-03-08 21:27:07 +0800

    Added login and signup functionality

---
+++ sessionmanager/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    CHOICES = [('U','User'),('B','Business Owner')]
+    account_type=forms.CharField(label='Account Type', widget=forms.RadioSelect(choices=CHOICES))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'account_type']
