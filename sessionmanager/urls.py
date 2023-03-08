d7e0308b155707c9eaa9b4b45f13ec1c62e45834
gabvariable <gabvariable@github.com>
2023-03-09 00:23:00 +0800

    Included functionality for logging out and added styling

--- sessionmanager/urls.py
+++ sessionmanager/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('loginuser/', auth_views.LoginView.as_view(template_name='sessionmanager/loginuser.html'), name='sessionmanager-loginuser'),
     path('loginbusinessowner/', auth_views.LoginView.as_view(template_name='sessionmanager/loginbusinessowner.html'), name='sessionmanager-loginbusinessowner'),
     path('signup/', views.signup, name='sessionmanager-signup'),
-    path('logout/', auth_views.LoginView.as_view(template_name='sessionmanager/logout.html'), name='sessionmanager-logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='sessionmanager/logout.html'), name='sessionmanager-logout'),
 ]
 
