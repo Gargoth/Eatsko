6cf14baac59d23dbe8c4d9cd29b6eab52de21848
Christian Jay F. Rosales <gargoth13@gmail.com>
2023-03-09 15:13:00 +0800

    add(userdashboard): Added styling

--- userdashboard/views.py
+++ userdashboard/views.py
@@ -45,13 +45,55 @@ context = {
 }
 
 def dashboard(request):
+    context['eateries'] = [
+            {
+                'name': 'Eatery Name',
+                'genre': 'Filipino',
+                'location': 'Area 2',
+                'rating': 5,
+            },
+            {
+                'name': 'Eatery Name',
+                'genre': 'Filipino',
+                'location': 'Area 2',
+                'rating': 5,
+            },
+            {
+                'name': 'Eatery Name',
+                'genre': 'Filipino',
+                'location': 'Area 2',
+                'rating': 5,
+            },
+            {
+                'name': 'Eatery Name',
+                'genre': 'Filipino',
+                'location': 'Area 2',
+                'rating': 5,
+            },
+            {
+                'name': 'Eatery Name',
+                'genre': 'Filipino',
+                'location': 'Area 2',
+                'rating': 5,
+            },
+            {
+                'name': 'Eatery Name',
+                'genre': 'Filipino',
+                'location': 'Area 2',
+                'rating': 5,
+            },
+    ]
+    context['page'] = 'dashboard'
     return render(request, 'userdashboard/dashboard.html', context)
 
 def profile(request):
+    context['page'] = 'profile'
     return render(request, 'userdashboard/profile.html', context)
 
 def campusmap(request):
+    context['page'] = 'campusmap'
     return render(request, 'userdashboard/campusmap.html', context)
 
 def findeatery(request):
+    context['page'] = 'findeatery'
     return render(request, 'userdashboard/findeatery.html', context)
