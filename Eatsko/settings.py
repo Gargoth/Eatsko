0cca249c19baf4bc345c7d7f890ff3888ed3432a
likaelvan <vedastovann@gmail.com>
2023-03-16 16:07:58 +0800

    Database Connection Test

--- Eatsko/settings.py
+++ Eatsko/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'Eatsko.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
